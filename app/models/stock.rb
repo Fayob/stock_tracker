fa92b7c369dd521a908ebc95256efe31ea4cbff7
fayob <adedejifavour6@gmail.com>
2023-03-14 19:25:12 +0100

    Update stock and user model

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,8 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks, dependent: :delete_all
+  has_many :users, through: :user_stocks, dependent: :delete_all
+
+  validates :name, :ticker, presence: true
 
   def self.new_lookup(ticker_symbol)
     # client = IEX::Api::Client.new(
@@ -8,11 +12,11 @@ class Stock < ApplicationRecord
     # )
     # client.price(ticker_symbol)
     company = Alphavantage::Fundamental.new(symbol: ticker_symbol)
-    client = Alphavantage::TimeSeries.new(symbol: ticker_symbol)
-    begin
-      new( ticker: ticker_symbol, name: company.overview.name, last_price: client.quote.price )
-    rescue => exception
-      return nil
-    end
+    client = Alphavantage::TimeSeries.new(symbol: ticker_symbol).quote
+    new( ticker: ticker_symbol, name: company.overview.name, last_price: client.price )
+  end
+
+  def self.check_db(ticker_symbol)
+    where(ticker: ticker_symbol).first
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ApplicationRecord
+  has_many :user_stocks, dependent: :delete_all
+  has_many :stocks, through: :user_stocks, dependent: :delete_all
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  def stock_already_tracked?(ticker_symbol)
+    stock = Stock.check_db(ticker_symbol)
+    return false unless stock
+    stocks.where(id: stock.id).exists?
+  end
+
+  def can_track_stock?(ticker_symbol)
+    !stock_already_tracked?(ticker_symbol)
+  end
 end
