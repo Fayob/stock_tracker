c27e399d8db75630d543d131f1908ec76f2f7a07
fayob <adedejifavour6@gmail.com>
2023-03-16 09:06:33 +0100

    Clean up stock model code

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,12 +5,6 @@ class Stock < ApplicationRecord
   validates :name, :ticker, presence: true
 
   def self.new_lookup(ticker_symbol)
-    # client = IEX::Api::Client.new(
-    #   publishable_token: Rails.application.credentials.iex_client[:api_key],
-    #   secret_token: Rails.application.credentials.iex_client[:secret_token],
-    #   endpoint: 'https://cloud.iexapis.com/v1'
-    # )
-    # client.price(ticker_symbol)
     company = Alphavantage::Fundamental.new(symbol: ticker_symbol)
     client = Alphavantage::TimeSeries.new(symbol: ticker_symbol).quote
     new( ticker: ticker_symbol, name: company.overview.name, last_price: client.price )
