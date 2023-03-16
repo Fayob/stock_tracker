54ca4b194218c3c1e8c8d47d87421b43e9126bd6
fayob <adedejifavour6@gmail.com>
2023-03-16 09:07:56 +0100

    Update stock controller functionality and page

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,6 +2,7 @@ class StocksController < ApplicationController
 
   def search
     if params[:stock].present?
+      @user = current_user
       @tracked_stocks = current_user.stocks
       @stock = Stock.new_lookup(params[:stock])
       if @stock.name && @stock.last_price
@@ -15,4 +16,13 @@ class StocksController < ApplicationController
       redirect_to my_portfolio_path
     end
   end
+
+  # def refresh
+  #   stocks = current_user.stocks
+  #   stocks.each do |stock|
+  #     new_stock = Stock.new_lookup(stock.ticker)
+  #     stock.last_price = new_stock.last_price
+  #   end
+  #   redirect_to my_portfolio_path
+  # end
 end
