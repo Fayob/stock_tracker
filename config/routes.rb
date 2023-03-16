20fb26305a55e4903693f14683d9c6d11445dac7
fayob <adedejifavour6@gmail.com>
2023-03-16 09:02:02 +0100

    Add route to refresh page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
-  resources :friends, only: [:index, :destroy]
   devise_for :users
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
+  get 'refresh_stock', to: 'stocks#refresh'
   get 'search_friend', to: 'friends#search'
+  resources :friends, only: [:index, :create, :destroy]
+  resources :users, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
